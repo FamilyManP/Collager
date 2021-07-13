692a4e293ff2a40ddffb5d422d4d2f932c686706
FamilyManP <87387972+FamilyManP@users.noreply.github.com>
2021-07-13 10:48:31 -0700

    Uploaded Tool

---
+++ Collager/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Collager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButton = new System.Windows.Forms.Button();
+            this.widthBox = new System.Windows.Forms.TextBox();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.badgesperColumn = new System.Windows.Forms.TextBox();
+            this.copyToClipboard = new System.Windows.Forms.CheckBox();
+            this.columnsLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.saveResult = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(235, 62);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(103, 31);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Select Images";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // widthBox
+            // 
+            this.widthBox.Location = new System.Drawing.Point(157, 36);
+            this.widthBox.Name = "widthBox";
+            this.widthBox.Size = new System.Drawing.Size(47, 20);
+            this.widthBox.TabIndex = 1;
+            this.widthBox.Text = "64";
+            this.widthBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.widthBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(157, 13);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(47, 20);
+            this.heightBox.TabIndex = 2;
+            this.heightBox.Text = "64";
+            this.heightBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // badgesperColumn
+            // 
+            this.badgesperColumn.Location = new System.Drawing.Point(39, 32);
+            this.badgesperColumn.Name = "badgesperColumn";
+            this.badgesperColumn.Size = new System.Drawing.Size(29, 20);
+            this.badgesperColumn.TabIndex = 3;
+            this.badgesperColumn.Text = "5";
+            this.badgesperColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // copyToClipboard
+            // 
+            this.copyToClipboard.AutoSize = true;
+            this.copyToClipboard.Location = new System.Drawing.Point(225, 15);
+            this.copyToClipboard.Name = "copyToClipboard";
+            this.copyToClipboard.Size = new System.Drawing.Size(113, 17);
+            this.copyToClipboard.TabIndex = 4;
+            this.copyToClipboard.Text = "Copy To Clipboard";
+            this.copyToClipboard.UseVisualStyleBackColor = true;
+            // 
+            // columnsLabel
+            // 
+            this.columnsLabel.AutoSize = true;
+            this.columnsLabel.Location = new System.Drawing.Point(9, 16);
+            this.columnsLabel.Name = "columnsLabel";
+            this.columnsLabel.Size = new System.Drawing.Size(101, 13);
+            this.columnsLabel.TabIndex = 5;
+            this.columnsLabel.Text = "Images Per Column ";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(116, 16);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(38, 13);
+            this.heightLabel.TabIndex = 6;
+            this.heightLabel.Text = "Height";
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Location = new System.Drawing.Point(116, 39);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(35, 13);
+            this.widthLabel.TabIndex = 7;
+            this.widthLabel.Text = "Width";
+            // 
+            // saveResult
+            // 
+            this.saveResult.AutoSize = true;
+            this.saveResult.Checked = true;
+            this.saveResult.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.saveResult.Location = new System.Drawing.Point(225, 39);
+            this.saveResult.Name = "saveResult";
+            this.saveResult.Size = new System.Drawing.Size(84, 17);
+            this.saveResult.TabIndex = 8;
+            this.saveResult.Text = "Save Result";
+            this.saveResult.UseVisualStyleBackColor = true;
+            // 
+            // Collager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 105);
+            this.Controls.Add(this.saveResult);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.columnsLabel);
+            this.Controls.Add(this.copyToClipboard);
+            this.Controls.Add(this.badgesperColumn);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.widthBox);
+            this.Controls.Add(this.runButton);
+            this.MaximumSize = new System.Drawing.Size(362, 144);
+            this.MinimumSize = new System.Drawing.Size(362, 144);
+            this.Name = "Collager";
+            this.Text = "Collager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox widthBox;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.TextBox badgesperColumn;
+        private System.Windows.Forms.CheckBox copyToClipboard;
+        private System.Windows.Forms.Label columnsLabel;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.CheckBox saveResult;
+    }
+}
+
---
+++ Collager/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Collager : Form
+    {
+        public Collager()
+        {
+            InitializeComponent();
+
+        }
+        int height;
+        int width;
+        int badgesPerColumn;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //set the variables from the GUI at runtime
+            //check if they error
+            try
+            {
+                height = int.Parse(heightBox.Text);
+                width = int.Parse(widthBox.Text);
+                badgesPerColumn = int.Parse(badgesperColumn.Text);
+            }
+            catch { MessageBox.Show("One or more of your values are not integers"); return; }
+
+            //File Picker Section
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //set parameters for the file dialog                
+                openFileDialog.Filter = "Image Files|*.PNG;*.JPG;*.GIF;*.TIFF";
+                openFileDialog.Title = "Select Pictures";
+                openFileDialog.Multiselect = true;
+                openFileDialog.RestoreDirectory = true;               
+
+                //when files are selected run the following
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //set an increment
+                    int i = 0;
+                    //this will be for building columns
+                    int currentWidth = 0;
+                    //find number of files user wants to use
+                    int filesSelected = openFileDialog.FileNames.Length;
+                    //MessageBox.Show("Files Selected: " + filesSelected.ToString());
+
+                    //check to see if the user selected less files than images per column so it doesn't create blank space
+                    if (filesSelected < badgesPerColumn)
+                    {
+                        badgesPerColumn = filesSelected;
+                    }
+                    
+                    //math for finding columns needed based upon allowed badges per column
+                    int columnsNeeded = (int)Math.Ceiling((double)filesSelected / (double)badgesPerColumn);
+                    //create an empty bitmap with the newly found values 
+                    Bitmap result = new Bitmap(width * columnsNeeded, height * badgesPerColumn);
+                    
+                    //iterate through each file selected
+                    foreach (string selectedItems in openFileDialog.FileNames)
+                    {
+                        //if the code has run through the number of badges per column, add the width to the current width so it moves over 1 
+                        if (i == badgesPerColumn)
+                        {
+                            currentWidth = currentWidth + width;
+                            i = 0;
+                        }
+                        //create graphics of the blank image
+                        Graphics newImage = Graphics.FromImage(result);
+                        
+                        //create a bitmap from the currently selected file in iteration
+                        Bitmap image = new Bitmap(selectedItems);
+                        
+                        //add the current bitmap in iteration to the previously blank image at the current coordinates 
+                        //use the current iteration for height multiplier 
+                        newImage.DrawImage(image, currentWidth, i * height, width, height);
+                        
+                        //check if the user wants this copied to their clipboard and do so 
+                        if (copyToClipboard.Checked == true)
+                        {
+                            Clipboard.SetImage(result);
+                        }
+
+                        //iterate
+                        i++;
+                        
+                    }
+
+                    //SAVE SECTION
+
+                    if (saveResult.Checked == true)
+                    {
+                        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                        saveFileDialog1.Filter = "Image Files|*.PNG;*.JPG;*.GIF;*.TIFF";
+                        saveFileDialog1.RestoreDirectory = true;
+
+                        if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                        {
+                            if (saveFileDialog1.FileName != null)
+                            {
+                                // Code to write the stream goes here.
+                                MessageBox.Show(saveFileDialog1.FileName);
+                                result.Save(saveFileDialog1.FileName, ImageFormat.Png);
+
+                            }
+                        }
+                    }
+                    
+                }
+                
+
+            }
+
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
